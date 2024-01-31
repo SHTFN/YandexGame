b9ce22f7782c1ebb8b712f1ead846ab62376249f
stefanalbu <salbu7701@gmail.com>
2024-01-31 23:18:12 +0300

    presentation

--- start_screen.py
+++ start_screen.py
@@ -8,24 +8,13 @@ class Start_screen:
         self.surface = surface
         self.clock = pygame.time.Clock()
 
-        text = ['Catch the diamond',
-                '',
-                'Press any key to start the game']
-
-        bg = pygame.transform.scale(pygame.image.load('data/tiles/start_screen/bg.jpg'), (WIDTH, HEIGHT))
+        bg = pygame.transform.scale(pygame.image.load('screen.png'), (WIDTH, HEIGHT))
         self.surface.blit(bg, (0, 0))
 
         # Рендер текста
         font = pygame.font.Font(None, 30)
-        text_coord = 50
-        for line in text:
-            string_rendered = font.render(line, 1, pygame.Color('white'))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 10
-            text_coord += intro_rect.height
-            self.surface.blit(string_rendered, intro_rect)
+        text = font.render('Press any key to start the game', 1, (255, 255, 255))
+        self.surface.blit(text, (170, 350))
 
     def run(self):
         while True:
